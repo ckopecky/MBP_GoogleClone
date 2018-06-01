f9545007f92b2969d6f24388f908a78d7c17b1e3
CMVNK <Kopecky12112@gmail.com>
2018-06-01 15:20:51 -0700

    main content on page
    no function

--- googleclone/src/Components/MainContent/MainContent.js
+++ googleclone/src/Components/MainContent/MainContent.js
@@ -6,8 +6,12 @@ class MainContent extends Component {
         return (
             <div className="main-content-wrapper">
                 <div className="search-area">
-                    <img className="google-logo" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"/>
-                    <input type="text"/>
+                    <img className="google-logo" alt="Google Logo" src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"/>
+                    <input className="input-search" type="text"/>
+                </div>
+                <div className ="button-container">
+                    <div className="search-btn">Google Search</div>
+                    <div className="search-btn">I'm Feeling Lucky</div>
                 </div>
             </div>
         );
